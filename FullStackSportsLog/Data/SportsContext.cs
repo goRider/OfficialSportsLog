4a9a2195b00955eaff78b759524cb4086ccbeb02
goRider <devgwo18@gmail.com>
2019-06-19 14:45:57 -0500

    Add project files.

---
+++ FullStackSportsLog/Data/SportsContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FullStackSportsLog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackSportsLog.Data
+{
+    public class SportsContext : DbContext
+    {
+        public SportsContext(DbContextOptions<SportsContext> options) : base(options) { }
+
+        public DbSet<AdminUser> AdminUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
